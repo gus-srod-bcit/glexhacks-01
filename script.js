c85ff28a48806df8eb289a47c82602b4d6120a35
Alex Lu <alu59@my.bcit.ca>
2026-01-08 14:49:45 -0800

    :feat: add anvil (not done)

--- script.js
+++ script.js
@@ -1,17 +1,66 @@
-resourceAmounts = {
-  "stone": 0,
-  "iron": 0,
-  "gold": 0,
-  "diamond": 0
+const WOODEN_PICKAXE = 0;
+const STONE_PICKAXE = 1;
+const IRON_PICKAXE = 2;
+const GOLD_PICKAXE = 3;
+const DIAMOND_PICKAXE = 4;
+
+var anvilResources = {
+  stone: {
+    level: STONE_PICKAXE,
+    cost: 10,
+    type: "stone"
+  },
+  iron: {
+    level: IRON_PICKAXE,
+    cost: 20,
+    type: "iron"
+  },
+  gold: {
+    level: GOLD_PICKAXE,
+    cost: 20,
+    type: "gold"
+  },
+  diamond: {
+    level: DIAMOND_PICKAXE,
+    cost: 20,
+    type: "diamond"
+  },
 };
 
+var resourceAmounts = {
+  stone: 0,
+  iron: 0,
+  gold: 0,
+  diamond: 0
+};
+
+var pickaxeLevel = WOODEN_PICKAXE;
+
+
+// Anvil Start
+const anvilMenuElement = document.getElementById("anvil-menu");
+function openAnvil() {
+  for (let resource in anvilResources) {
+    let resourceCraft = document.getElementById("anvil-craft-template");
+    resourceCraft.querySelector()
+  }
+
+  anvilMenuElement.showModal();
+}
+
+document.getElementById("anvil-open-button").addEventListener("click", openAnvil)
+document.getElementById("anvil-close-button").addEventListener("click", anvilMenuElement.close)
+
+// Anvil End
+
+
+// Mining Start
 function gainResource(event) {
   var _resourceElement = event.target.closest(".resource");
   if (!_resourceElement) return;
 
 
   for (let resource in resourceAmounts) {
-    console.log(resource);
     if (_resourceElement.classList.contains(resource)) {
       resourceAmounts[resource] += 1;
       var _resourceAmountElement = document.getElementById(`${resource}-amount`)
@@ -20,6 +69,5 @@ function gainResource(event) {
     }
   }
 }
-
 
 document.addEventListener("click", gainResource);
